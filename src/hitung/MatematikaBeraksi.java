7ef49317dce8a315f6ba18be9e5e50e645b7d539
HibatID <muhammadhibatullah@gmail.com>
2023-06-12 09:29:11 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -9,5 +9,11 @@ package hitung;
  * @author TUF GAMING FX506II
  */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika hibat = new Matematika(9,9);
+        System.out.println("Hasil Penjumlahan : " + hibat.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : " + hibat.setPengurangan());
+        System.out.println("Hasil Perkalian : " + hibat.setPerkalian());
+        System.out.println("Hasil Pembagian : " + hibat.setPembagian());
+    }
 }
