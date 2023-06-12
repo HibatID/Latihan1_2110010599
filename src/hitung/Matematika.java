db1d9f10e472108f01fc6080ea2481511ae6a0fb
HibatID <muhammadhibatullah@gmail.com>
2023-06-12 09:25:01 +0800

    Melengkapi method matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -9,17 +9,27 @@ package hitung;
  * @author TUF GAMING FX506II
  */
 public class Matematika {
+
     //membuat variabel
     private double bil1, bil2;
-    
+
     //membuat constructor
-    public Matematika(double bil1, double bil2){
+    public Matematika(double bil1, double bil2) {
         this.bil1 = bil1;
         this.bil2 = bil2;
     }
-    
+
     //membuat method setPenjumlahan
-    public double setPenjumlahan(){
+    public double setPenjumlahan() {
         return bil1 + bil2;
+    }
+    public double setPengurangan() {
+        return bil1 - bil2;
+    }
+    public double setPerkalian() {
+        return bil1 * bil2;
+    }
+    public double setPembagian() {
+        return bil1 / bil2;
     }
 }
